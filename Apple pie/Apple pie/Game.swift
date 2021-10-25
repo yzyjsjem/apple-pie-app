f96f1d8fbd5f702fd33455172bac317101f92e9a
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2021-10-25 16:23:36 +1100

    add comments

--- Apple pie/Apple pie/Game.swift
+++ Apple pie/Apple pie/Game.swift
@@ -8,26 +8,26 @@
 import Foundation
 
 struct Game {
-    var word : String
-    var incorrectmovesremaining : Int
-    var guessedletters : [Character]
+    var word : String   //correct answer
+    var incorrectmovesremaining : Int         //how many moves left
+    var guessedletters : [Character]        //the player guessedletter
     var formatword : String {
-        var guessedword = ""
+        var guessedword = "" //guessedword stores all the word guessed correctly
         for letter in word {
-            if guessedletters.contains(letter) {
+            if guessedletters.contains(letter) {   //if the guessedletters has some words guessed right, add them to gussedword
                 guessedword += "\(letter)"
             }
             else{
-                guessedword += "_"
+                guessedword += "_"  //if guessed wrong, show as a "_"
             }
         }
-        return guessedword
+        return guessedword //for example, the correct answer is one, you tapped word is anmle(guessedletters) the guessedword will be -n- ( the number of "_" the original length of word
     }
     
     
     mutating func playerguessed(letter: Character) {
-        guessedletters.append(letter)
-        if !word.contains(letter){
+        guessedletters.append(letter)  //add the new tapped letter to the guessedletters
+        if !word.contains(letter){    //if the letter is wrong then delete one move left
             incorrectmovesremaining -= 1
             
         }
--- Apple pie/Apple pie/ViewController.swift
+++ Apple pie/Apple pie/ViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var treeimageview: UIImageView!
-    @IBOutlet weak var correctwordlabel: UILabel!
-    @IBOutlet weak var scorelabel: UILabel!
+    @IBOutlet weak var treeimageview: UIImageView! //the tree's image
+    @IBOutlet weak var correctwordlabel: UILabel!  //the label shows the correctword
+    @IBOutlet weak var scorelabel: UILabel! //the label shows how many times win or lose
     
-    @IBOutlet var letterbuttons: [UIButton]!
+    @IBOutlet var letterbuttons: [UIButton]! //connect with all the buttons
     
-    var listofwords = ["one", "eleven", "twentytwo", "thirtythree", "fiftyfive", "sixtysix"]
-    let incorrectmovesallowed = 7
+    var listofwords = ["one", "eleven", "twentytwo", "thirtythree", "fiftyfive", "sixtysix"]   //an array store all words for the game
+    let incorrectmovesallowed = 7 //How many times to lose.
     var totalwins = 0 {
         didSet {
             newround()
@@ -27,14 +27,14 @@ class ViewController: UIViewController {
             newround()
         }
     }
-    var currentgame : Game!
+    var currentgame : Game! // currentgame could have no value at first until videwdidload()
     
     
     @IBAction func buttonpressed(_ sender: UIButton) {
-        sender.isEnabled = false
-        let letterstring = sender.title(for: .normal)!
-        let letter = Character(letterstring.lowercased())  //letter是按钮选择的字母
-        currentgame.playerguessed(letter: letter)
+        sender.isEnabled = false //once pressed, the button can't be pressed until next round
+        let letterstring = sender.title(for: .normal)! //get sender's title
+        let letter = Character(letterstring.lowercased())  // turn the title to lowercase
+        currentgame.playerguessed(letter: letter) //use the letter(tapped button) in palyerguessed to check if right
         updategamestate()
     }
     
@@ -44,50 +44,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        newround()
+        newround()   //after the view loaded, start a new round
     }
     
     func newround() {
-        if !listofwords.isEmpty {
-            let newword = listofwords.removeFirst()
+        if !listofwords.isEmpty {   //if there is still words haven't been used in the list
+            let newword = listofwords.removeFirst() //get the frist word from the list can use that start a new game
             currentgame = Game(word: newword, incorrectmovesremaining: incorrectmovesallowed, guessedletters: [])
-            enableletterbuttons(true)
+            enableletterbuttons(true)     //turn all buttons to work
             updateui()
         }
         else{
-            enableletterbuttons(false)
+            enableletterbuttons(false)  // there will be no newround and the buttons still don't work
         }
     }
     
-    func enableletterbuttons(_ enable: Bool){
+    func enableletterbuttons(_ enable: Bool){     //let buttons work again
         for button in letterbuttons{
             button.isEnabled = enable
         }
     }
     
     func updateui() {
-        var letters = [String]()
-        for letter in currentgame.formatword {
+        var letters = [String]() // set a blank array named letters
+        for letter in currentgame.formatword { //add formatword from currentgame to the array
             letters.append(String(letter))
         }
-        let wordwithspacing = letters.joined(separator: " ")
+        let wordwithspacing = letters.joined(separator: " ") //put letters together but have a space between them
         
         
-        correctwordlabel.text = wordwithspacing
-        scorelabel.text = "Wins: \(totalwins), Losses: \(totallosses)"
-        treeimageview.image = UIImage(named: "Tree \(currentgame.incorrectmovesremaining)")
+        correctwordlabel.text = wordwithspacing //update the correctword label
+        scorelabel.text = "Wins: \(totalwins), Losses: \(totallosses)"      //update the scorelabel
+        treeimageview.image = UIImage(named: "Tree \(currentgame.incorrectmovesremaining)") //update the tree image
         
     }
     
-    func updategamestate(){
+    func updategamestate(){  //check after press the button
         if currentgame.incorrectmovesremaining == 0 {
-            totallosses += 1
+            totallosses += 1            //if no moves left it will lose
         }
         else if currentgame.word == currentgame.formatword {
-            totalwins += 1
+            totalwins += 1                //or the word is complete correctly, it will win
         }
         else {
-            updateui()
+            updateui()   // or update the game and continue
         }
         
     }
